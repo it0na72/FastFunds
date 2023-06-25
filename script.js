8219a5cb4d3991f209487050add065d5d765f385
Andrew <xgearh3h3@gmail.com>
2023-06-25 21:53:13 +0100

    changed title of the application

--- script.js
+++ script.js
@@ -497,3 +497,12 @@ console.log(movements);
 */
 
 // coding challenge 4
+
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Alice', 'Bob'] },
+];
+
+dogs.forEach(dog => (dog.recFood = dog.weight ** 0.75 * 28));
